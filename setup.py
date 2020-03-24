fb49f0dc70ba0c0c5d304bdd25c8e42d8695c7f4
perfide <github.perfide@safersignup.com>
2020-03-24 00:00:00 +0000

    chore: add files for setuptools

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Copyright:
+#   2020 P. H. <github.com/perfide>
+# License:
+#   BSD-2-Clause (BSD 2-Clause "Simplified" License)
+#   https://spdx.org/licenses/BSD-2-Clause.html
+
+"""Stub-script required by setuptools"""
+
+import setuptools
+
+setuptools.setup()
